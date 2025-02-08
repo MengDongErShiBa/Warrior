3783701adf8f69393fc3f4b91e1e2cc19fb6a8e8
麻婆大豆腐。 <wangshuai9595@163.com>
2025-02-08 21:49:29 +0800

    21：重击逻辑，增加BlueprintLibrary

---
+++ Source/Warrior/Private/WarriorBlueprintFunctionLibrary.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WarriorBlueprintFunctionLibrary.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+
+UWarriorAbilitySystemComponent* UWarriorBlueprintFunctionLibrary::NativeGetWarriorASCFromActor(AActor* InActor)
+{
+	check(InActor);
+	return CastChecked<UWarriorAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+}
+
+void UWarriorBlueprintFunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorASCFromActor(InActor);
+
+	// 检查是否包含标签
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		ASC->AddLooseGameplayTag(TagToAdd);
+	}
+}
+
+void UWarriorBlueprintFunctionLibrary::RemoveGameplayFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorASCFromActor(InActor);
+
+	if (ASC->HasMatchingGameplayTag(TagToRemove))
+	{
+		ASC->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+bool UWarriorBlueprintFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorASCFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+void UWarriorBlueprintFunctionLibrary::BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck,
+	EWarriorConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor, TagToCheck) ? EWarriorConfirmType::Yes : EWarriorConfirmType::No;
+}
---
+++ Source/Warrior/Public/WarriorBlueprintFunctionLibrary.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WarriorBlueprintFunctionLibrary.generated.h"
+
+struct FGameplayTag;
+class UWarriorAbilitySystemComponent;
+
+UENUM()
+enum class EWarriorConfirmType : uint8
+{
+	Yes,
+	No
+};
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * 获取ASC组件
+	 * @param InActor 
+	 * @return 
+	 */
+	static UWarriorAbilitySystemComponent* NativeGetWarriorASCFromActor(AActor* InActor);
+
+	/**
+	 * 添加Tag，如果Tag不存在
+	 * @param InActor 
+	 * @param TagToAdd 
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd);
+
+	/**
+	 * 如果可以找到Tag则删除Actor对应的Tag
+	 * @param InActor 
+	 * @param TagToRemove  
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void RemoveGameplayFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove);
+
+	/**
+	 * 是否拥有标签
+	 * @param InActor 
+	 * @param TagToCheck 
+	 * @return 
+	 */
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+	/**
+	 * 是否包含某个标签
+	 * @param InActor 
+	 * @param TagToCheck 
+	 * @param OutConfirmType 返回类型 
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary", meta = (DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EWarriorConfirmType& OutConfirmType);
+};
