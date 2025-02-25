fac328171b37ebc9dae0da4b0c8e0ab58bd8fe73
麻婆大豆腐。 <wangshuai9595@163.com>
2025-02-25 16:32:08 +0800

    5.21  Notify Enemy Melee Hit

--- Source/Warrior/Private/Component/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Component/Combat/EnemyCombatComponent.cpp
@@ -3,12 +3,50 @@
 
 #include "Component/Combat/EnemyCombatComponent.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "WarriorDebugHelper.h"
+#include "WarriorGameplayTags.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	if (OverlappedActors.Contains(HitActor))
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		return;
 	}
+
+	// 在禁用武器碰撞部分，做出了清理操作
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO: Implement block check 执行阻挡检查
+	// 是否有效阻挡
+	bool bIsValidBlock = false;
+
+	// 玩家正在阻挡
+	const bool bIsPlayerBlacking = false;
+	// 攻击是否为不可阻挡
+	const bool bIsMyAttackUnBlockAble = false;
+
+	if (bIsPlayerBlacking && !bIsMyAttackUnBlockAble)
+	{
+		// TODO: check if the block is valid
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+		// 有效阻挡
+	}
+	else
+	{
+		// 应用伤害
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
+	}
+	
 }
