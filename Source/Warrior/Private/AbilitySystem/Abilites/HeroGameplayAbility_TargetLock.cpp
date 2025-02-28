e1fea46484744889675197f26d2ad65f147a513b
麻婆大豆腐。 <wangshuai9595@163.com>
2025-02-28 19:27:19 +0800

    6.25：通过辅助函数库来捕捉可锁定目标

--- Source/Warrior/Private/AbilitySystem/Abilites/HeroGameplayAbility_TargetLock.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilites/HeroGameplayAbility_TargetLock.cpp
@@ -3,10 +3,15 @@
 
 #include "AbilitySystem/Abilites/HeroGameplayAbility_TargetLock.h"
 
+#include "WarriorDebugHelper.h"
+#include "Characters/WarriorHeroCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+                                                      const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                                      const FGameplayEventData* TriggerEventData)
 {
+	TryLockOnTarget();
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
@@ -15,4 +20,41 @@ void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandl
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+	TArray<FHitResult> BoxTraceHits;
+
+	// 盒子trace
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + GetHeroCharacterFromActorInfo()->GetActorForwardVector() * TraceDistance,
+		TraceBoxSize / 2.f,
+		GetHeroCharacterFromActorInfo()->GetActorForwardVector().ToOrientationRotator(),
+		BoxTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bShowPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+		BoxTraceHits,
+		true
+	);
+
+	for (const FHitResult& TraceHit : BoxTraceHits)
+	{
+		if (AActor* HitActor = TraceHit.GetActor())
+		{
+			if (HitActor != GetHeroCharacterFromActorInfo())
+			{
+				AvailableActorsToLock.AddUnique(HitActor);
+				Debug::Print(HitActor->GetActorNameOrLabel());
+			}
+		}
+	}
 }
--- Source/Warrior/Public/AbilitySystem/Abilites/HeroGameplayAbility_TargetLock.h
+++ Source/Warrior/Public/AbilitySystem/Abilites/HeroGameplayAbility_TargetLock.h
@@ -19,4 +19,30 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	// ~ End UGameplayAbility Interface
+
+private:
+	/**
+	 * 尝试锁定目标
+	 */
+	void TryLockOnTarget();
+
+	/**
+	 * 获取可用锁定的Actors
+	 */
+	void GetAvailableActorsToLock();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace Lock")
+	float TraceDistance = 5000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	FVector TraceBoxSize = FVector(5000.f, 5000.f, 300.f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TArray<TEnumAsByte<EObjectTypeQuery>> BoxTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	bool bShowPersistentDebugShape;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TArray<AActor*> AvailableActorsToLock;
 };
